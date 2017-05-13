9990d48f45e051346bd1ae40305338dc6c687482
Alethea Rose <alethea@alethearose.com>
2017-05-13 21:55:20 +0000

    chore: set up repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# m17n - Multilingualization
+
+A suite of utilities for supporting multiple languages in your apps
+
+[![Build Status](https://travis-ci.org/alethea/m17n.svg?branch=master)](https://travis-ci.org/alethea/m17n)
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
+# Developing
+The m17n project is magaged as a [Lerna](https://lernajs.io/) monorepo. Contributors are expected to adhere to our [Code of Conduct](https://github.com/alethea/m17n/tree/master/CODE_OF_CONDUCT.md).
---
+++ packages/m17n/index.js
@@ -0,0 +1,20 @@
+/**
+ * @file m17n
+ * @author Alethea Rose <alethea@alethearose.com>
+ * @copyright Copyright © Alethea Rose, 2017.
+ * @license AGPL-3.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module.exports = {}
